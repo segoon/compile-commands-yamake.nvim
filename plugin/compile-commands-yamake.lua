7fc5ff1a6630742553863b41a0b18e5511e216d9
Vasily Kulikov <segoon@yandex-team.ru>
2025-05-03 03:04:48 +0300

    codegen files

--- plugin/compile-commands-yamake.lua
+++ plugin/compile-commands-yamake.lua
@@ -1,32 +1,42 @@
-local function ask_for_compile_commands()
-      local root = vim.fs.root(0, {'service.yaml', 'codegen-module.yaml'})
-      if not root then
-	root = vim.fs.root(0, {'ya.make'})
-      end
-      if not root then
-        -- Not arcadia
-        return
-      end
-      if vim.fs.root(0, {'compile_commands.json'}) then
-        -- Already generated
-        return
-      end
+local function generate_compile_commands(root, with_codegen)
+  print('\nGenerating compile_commands.json...')
+  if with_codegen then
+    vim.system({'ya', 'make', '--add-result=', '--replace-result'}, function(obj)
+      vim.schedule(function() vim.cmd.echo({'"Codegen files are generated"'}) end)
+    end)
+  end
 
-      vim.ui.input({prompt = 'Generate compile_commands.json for ' .. root .. '? [Y/n]'}, 
-        function(input)
-          if input == '' or input == 'y' then
-	    print('\nGenerating compile_commands.json...')
-            vim.system({'ya', 'dump', 'compile_commands'},
-	      function(obj)
-                local file = io.open(root .. '/compile_commands.json', 'w')
-                file:write(obj.stdout)
-                file:close()
-		vim.schedule(function() vim.cmd.echo({'"compile_commands.json is generated"'}) end)
-	      end
-            )
-          end
-        end
-      )
+  vim.system({'ya', 'dump', 'compile_commands'},
+    function(obj)
+      local file = io.open(root .. '/compile_commands.json', 'w')
+      file:write(obj.stdout)
+      file:close()
+      vim.schedule(function() vim.cmd.echo({'"compile_commands.json is generated"'}) end)
+    end
+  )
+end
+
+local function ask_for_compile_commands()
+  local root = vim.fs.root(0, {'service.yaml', 'codegen-module.yaml'})
+  if not root then
+    root = vim.fs.root(0, {'ya.make'})
+  end
+  if not root then
+    -- Not arcadia
+    return
+  end
+  if vim.fs.root(0, {'compile_commands.json'}) then
+    -- Already generated
+    return
+  end
+ 
+  vim.ui.input({prompt = 'Generate compile_commands.json for ' .. root .. '? [Y/n]'}, 
+    function(input)
+      if input == '' or input == 'y' then
+        generate_compile_commands(root, true)
+      end
+    end
+  )
 end
 
 vim.api.nvim_create_autocmd(
